569e157bd1166e45455b94ecc78ad83ad4043a10
LucaGiona <luca@senzproduction.com>
2026-08-19 19:58:31 +0200

    Landing Page fpr Quizzes

---
+++ js/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    title: "Frau F. muss umziehen",
+    description:
+      "Quiz zu Auge, Ohr, Pflegeversicherung, Pflegegraden und Entlassungsmanagement.",
+    topics: [
+      "Auge",
+      "Ohr",
+      "Pflegeversicherung",
+      "Pflegegrade",
+      "Entlassungsmanagement",
+    ],
+    status: "verfügbar",
+    href: "./frau-f-muss-umziehen/index.html",
+  },
+];
